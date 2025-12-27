5b9a540f09c537df9a525df0bacdb43e36367298
mamaarez <149959190+mamaarez@users.noreply.github.com>
2025-12-28 01:36:32 +0330

    first bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 ربات با موفقیت آنلاین شد!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
